aaad312064860b9069983ccc6363c1a788081c77
Sambalicious <samuel@sendme.ng>
2022-12-01 04:40:08 +0100

    install dependencies

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,6 +3,7 @@ import Image from 'next/image'
 import styles from '../styles/Home.module.css'
 
 export default function Home() {
+  console.log('hello world')
   return (
     <div className={styles.container}>
       <Head>
